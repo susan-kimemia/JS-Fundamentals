4c42765de817aa0d6d10163897f9d4977c4baaf3
Susan <susankimemia111@gmail.com>
2025-09-16 14:30:12 +0300

    Add 6-multi_languages_loop.js to print languages using a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// 6-multi_languages_loop.js
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
+ 
