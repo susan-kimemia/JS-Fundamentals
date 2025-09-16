765ba0e498962c84aa2a5d16d8e7e928b289dd4d
Susan <susankimemia111@gmail.com>
2025-09-16 14:37:28 +0300

    Fix 7-multi_c.js to handle negative numbers correctly

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,14 +1,14 @@
+ 
 // 7-multi_c.js
 const args = process.argv;
 const x = parseInt(args[2], 10);
 
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
   let i = 0;
-  while (i < x) {
+  while (i < Math.abs(x)) { // absolute value ensures negative numbers print correctly
     console.log("C is fun");
     i++;
   }
 }
- 
