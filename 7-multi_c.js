296b0289d5b5918fb51baedf4ab0393d6ca8ecc1
Susan <susankimemia111@gmail.com>
2025-09-16 14:40:35 +0300

    Fix 7-multi_c.js: print only for positive numbers, handle missing input

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,14 +1,14 @@
- 
 // 7-multi_c.js
 const args = process.argv;
 const x = parseInt(args[2], 10);
 
 if (isNaN(x)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (x > 0) {
   let i = 0;
-  while (i < Math.abs(x)) { // absolute value ensures negative numbers print correctly
+  while (i < x) {
     console.log("C is fun");
     i++;
   }
 }
+
